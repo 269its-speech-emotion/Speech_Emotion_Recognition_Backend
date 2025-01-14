ee1582351a37e4b33f7f70889244568061c0267b
idrisseaa <idrisseaa93@gmail.com>
2025-01-14 18:34:10 +0100

    Implemented the AppUserDetailsService

--- src/main/java/com/kmits/projects/speechemotionrecognition/repositories/AppUserRepository.java
+++ src/main/java/com/kmits/projects/speechemotionrecognition/repositories/AppUserRepository.java
@@ -2,9 +2,11 @@ package com.kmits.projects.speechemotionrecognition.repositories;
 
 import com.kmits.projects.speechemotionrecognition.entities.AppUser;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
 
+@Repository
 public interface AppUserRepository extends JpaRepository<AppUser, Long> {
     Optional<AppUser> findByEmail(String email);
     Optional<AppUser> findByUsername(String username);
