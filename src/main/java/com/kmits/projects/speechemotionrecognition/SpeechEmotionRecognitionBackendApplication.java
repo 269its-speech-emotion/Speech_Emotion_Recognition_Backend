a67909baec8fc1e92be55f1c64df54f130174524
idrisseaa <idrisseaa93@gmail.com>
2025-01-14 13:05:32 +0100

    Initial commit: Spring Boot Speech Emotion Recognition Backend

---
+++ src/main/java/com/kmits/projects/speechemotionrecognition/SpeechEmotionRecognitionBackendApplication.java
@@ -0,0 +1,13 @@
+package com.kmits.projects.speechemotionrecognition;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpeechEmotionRecognitionBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpeechEmotionRecognitionBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/kmits/projects/speechemotionrecognition/SpeechEmotionRecognitionBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kmits.projects.speechemotionrecognition;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpeechEmotionRecognitionBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
